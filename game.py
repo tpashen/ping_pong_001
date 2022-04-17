ce1b2f06d38f504010a35d6e87bc6ff6e4c1738d
MakaGT <103880074+MakaGT@users.noreply.github.com>
2022-04-17 13:11:18 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,17 @@
 #1
-#2
+#2class Player(GameSprite): 
+    def update_r(slef):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5: 
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+    keys = key.gget_pressed()
+    if keys[K_w] and self.rect.y > 5:
+        self.rect.y -= self.speed
+    if keys[K_s] and self.rect.y < win_height - 80:
+        self.rect.y += self.speed
 #3
 #4
 #5
