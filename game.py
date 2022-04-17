c87d968c2544094a11e8cc96c062d34b6359f26f
vladshorf2 <103879425+vladshorf2@users.noreply.github.com>
2022-04-17 12:21:58 +0200

    Update game.py

--- game.py
+++ game.py
@@ -13,5 +13,17 @@
     if keys[K_s] and self.rect.y < win_height - 80:
         self.rect.y += self.speed
 #3
-#4
+#4while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
 #5
