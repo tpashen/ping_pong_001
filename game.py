e16565c502dfa0f69e6ed284430ae736cb0780eb
gosha12600 <103879186+gosha12600@users.noreply.github.com>
2022-04-23 12:24:50 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,19 @@
 #1
+from pygame import*
+
+class GameSprite(sprite.sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 #2class Player(GameSprite): 
     def update_r(slef):
         keys = key.get_pressed()
